109cb806e682bd32813e50887fc20c9335692df8
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-02-18 23:10:07 -0700

    xFix
    Simple Lua Script to clean up stuck XTarget slots.
    
    Checks to make sure we are not in combat.
    Scans for any names in Xtarget list that have no health.
    If any are found it resets that slot to autohater.

---
+++ init.lua
@@ -0,0 +1,34 @@
+-- xFix Lua
+
+	-- Name: XFix
+	-- Author: Grimmier
+	-- Purpose: fix stuck xtarget slots that will break automation. Sometimes these are empty slots other times they are corpses.
+	-- Scans xtarg list every second. if we are not in combat and there is a stuck slot we pop it and set it back to autohater.
+
+---@type Mq
+local mq = require('mq')
+
+local function ScanXtar()
+	if mq.TLO.Me.XTarget() > 0 and mq.TLO.Me.CombatState() ~= 'COMBAT' then
+		for i = 1, 20 do
+			if mq.TLO.Me.XTarget() > 0 and mq.TLO.Me.CombatState() ~= 'COMBAT' then
+				local xName = mq.TLO.Me.XTarget(i).Name() or 'NULL'
+				local xHp = mq.TLO.Me.XTarget(i).PctHPs() or -1
+				local xType = mq.TLO.Me.XTarget(i).Type() or '?'
+				if ((xName ~= 'NULL' and xHp == -1 ) or xType == 'Corpse') then
+					mq.cmd('/squelch /xtarg set '..i..' AH')
+					print(':: xFix :: Cleaning Xtarget Slot:: '..i..' Name:: '..xName..' Type:: '..xType)
+				end
+			else
+				break
+			end
+		end
+	end
+end
+local function loop()
+	while true do
+		ScanXtar()
+		mq.delay('1s')
+	end
+end
+loop()
