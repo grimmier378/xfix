abb5e83a0641f59c57ba4799bc2c045e5796b0b6
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-02-19 22:46:07 -0700

    feature: commands
    xFix:: LOADING xFix::Version::1.5
    xFix:: xFix Command List:
    xFix:: /xfix pause  Toggles Pause on and off
    xfix:: /xfis debug  Turn on and off Debug Spam
    xFix:: /xfix stop  Ends the script
    xFix:: /xfix help  Lists this command list

--- init.lua
+++ init.lua
@@ -1,14 +1,53 @@
 -- xFix Lua
 
 	-- Name: XFix
-	-- Version: 1.2
+	-- 
 	-- Author: Grimmier
 	-- Purpose: fix stuck xtarget slots that will break automation. Sometimes these are empty slots other times they are corpses.
 	-- Scans xtarg list every second. if we are not in combat and there is a stuck slot we pop it and set it back to autohater.
 
 ---@type Mq
 local mq = require('mq')
-
+local PAUSED = false
+local DEBUG = false
+local RUNNING = true
+local version = 1.5
+local function debug(string)
+    if(DEBUG) then print(string.format('\aoxFix::\aoDEBUG::\at %s',tostring(string))) end
+end
+local function help()
+	print('\ayxFix::\a-o xFix Command List:')
+	print('\ayxFix::\ag /xfix pause \at Toggles Pause on and off')
+	print('\ayxfix::\ag /xfis debug \at Turn on and off Debug Spam')
+	print('\ayxFix::\ag /xfix stop \at Ends the script')
+	print('\ayxFix::\ag /xfix help \at Lists this command list')
+end
+local function bind(...)
+    local args = {...}
+    local key = args[1]
+    local value = args[2]
+    if key == 'pause' then
+       	if PAUSED then 
+			PAUSED = false
+			print('\ayxFix::\ag xFix Resuming...')
+		else
+			PAUSED = true
+			print('\ayxFix::\ao xFix Paused!')
+		end
+	elseif key == 'debug' then
+        if DEBUG then
+			 DEBUG = false 
+      	  	print('\ayxFix::\ao Debug Spam Disabled!')
+		else
+			DEBUG = true
+			print('\ayxFix::\ag Debug Spam Enabled!')
+		end
+	elseif key == 'help' or not key then
+		help()
+    elseif key == 'stop' then
+		RUNNING = false
+	end
+end
 local function ScanXtar()
 	if mq.TLO.Me.XTarget() > 0 then
 		for i = 1, 20 do
@@ -22,7 +61,8 @@ local function ScanXtar()
 					mq.cmd('/squelch /xtarg set '..i..' ET')
 					mq.delay(100)
 					mq.cmd('/squelch /xtarg set '..i..' AH')
-				--	print(':: xFix :: Cleaning Xtarget Slot:: '..i..' Name:: '..xName..' Type:: '..xType)
+				local debugString = ' Cleaning Xtarget Slot:: '..i..' Name:: '..xName..' Type:: '..xType
+				if DEBUG then debug(debugString) end
 				end
 			else
 				break
@@ -30,10 +70,18 @@ local function ScanXtar()
 		end
 	end
 end
+local function init()
+	print('\ayxFix::\ag LOADING \ayxFix::Version::\ag'..tostring(version))
+	mq.bind('/xfix', bind)
+	help()
+end
 local function loop()
-	while true do
-		if mq.TLO.Me.CombatState() ~= 'COMBAT' then ScanXtar() end
+	while RUNNING do
+		if not PAUSED then
+			if mq.TLO.Me.CombatState() ~= 'COMBAT' then ScanXtar() end
+		end
 		mq.delay('1s')
 	end
 end
+init()
 loop()
