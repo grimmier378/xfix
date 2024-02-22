9748b51fa24d7ef82d9bf21d3bc4750fcf89aa52
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-02-22 16:42:17 -0700

    Fix for corpses.
    * Should be final version. Now properly pops corpses.

--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
 -- xFix Lua
 
-	-- Name: XFix
-	-- 
-	-- Author: Grimmier
-	-- Purpose: fix stuck xtarget slots that will break automation. Sometimes these are empty slots other times they are corpses.
-	-- Scans xtarg list every second. if we are not in combat and there is a stuck slot we pop it and set it back to autohater.
+-- Name: XFix
+--
+-- Author: Grimmier
+-- Purpose: fix stuck xtarget slots that will break automation. Sometimes these are empty slots other times they are corpses.
+-- Scans xtarg list every second. if we are not in combat and there is a stuck slot we pop it and set it back to autohater.
 
 local mq = require('mq')
 local PAUSED = false
 local DEBUG = false
 local RUNNING = true
-local version = "1.7.2"
+local version = "1.8"
 local arg = {...}
 if arg[1] and arg[1] == 'debug' then DEBUG = true end
 local function debug(string)
-    if(DEBUG) then print(string.format('\aoxFix::\aoDEBUG::\at %s',tostring(string))) end
+	if(DEBUG) then print(string.format('\aoxFix::\aoDEBUG::\at %s',tostring(string))) end
 end
 local function help()
 	print('\ayxFix::\a-o xFix Command List:')
@@ -25,28 +25,28 @@ local function help()
 	if DEBUG then print('\ayxFix::\ag Debug Spam Enabled!') end
 end
 local function bind(...)
-    local args = {...}
-    local key = args[1]
-    local value = args[2]
-    if key == 'pause' then
-       	if PAUSED then 
+	local args = {...}
+	local key = args[1]
+	local value = args[2]
+	if key == 'pause' then
+		if PAUSED then
 			PAUSED = false
 			print('\ayxFix::\ag xFix Resuming...')
-		else
+			else
 			PAUSED = true
 			print('\ayxFix::\ao xFix Paused!')
 		end
-	elseif key == 'debug' then
-        if DEBUG then
-			 DEBUG = false 
-      	  	print('\ayxFix::\ao Debug Spam Disabled!')
-		else
+		elseif key == 'debug' then
+		if DEBUG then
+			DEBUG = false
+			print('\ayxFix::\ao Debug Spam Disabled!')
+			else
 			DEBUG = true
 			print('\ayxFix::\ag Debug Spam Enabled!')
 		end
-	elseif key == 'help' or not key then
+		elseif key == 'help' or not key then
 		help()
-    elseif key == 'stop' then
+		elseif key == 'stop' then
 		RUNNING = false
 	end
 end
@@ -54,19 +54,20 @@ local function ScanXtar()
 	if mq.TLO.Me.XTarget() > 0 then
 		for i = 1, mq.TLO.Me.XTargetSlots() do
 			local xTarg = mq.TLO.Me.XTarget(i)
-			if xTarg.ID() > 0 and xTarg.Type() ~= 'Corpse' then return end
-			local xCount = mq.TLO.Me.XTarget() or 0
-			local xName, xType = xTarg.Name(), xTarg.Type()
-			if (xCount > 0 and xTarg.ID() == 0) or (xType == 'Corpse') then
-				if ((xTarg.Name() ~= 'NULL' and xTarg.ID() == 0) or (xType == 'Corpse')) then
-					mq.cmdf("/squelch /xtarg set %s ET", i)
-					mq.delay(100)
-					mq.cmdf("/squelch /xtarg set %s AH", i)
-					local debugString = string.format('\ayxFix\aw:: Cleaning Xtarget Slot::\at %s\aw XTarget Count::\ao %s\aw Name::\ag %s\aw Type:: \t%s', i, xCount, xName, xType)
-					if DEBUG then debug(debugString) end
+			if not (xTarg.ID() > 0 and xTarg.Type() ~= 'Corpse') then
+				local xCount = mq.TLO.Me.XTarget() or 0
+				local xName, xType = xTarg.Name(), xTarg.Type()
+				if (xCount > 0 and xTarg.ID() == 0) or (xType == 'Corpse') then
+					if ((xTarg.Name() ~= 'NULL' and xTarg.ID() == 0) or (xType == 'Corpse')) then
+						mq.cmdf("/squelch /xtarg set %s ET", i)
+						mq.delay(100)
+						mq.cmdf("/squelch /xtarg set %s AH", i)
+						local debugString = string.format('\ayxFix\aw:: Cleaning Xtarget Slot::\at %s\aw XTarget Count::\ao %s\aw Name::\ag %s\aw Type:: \t%s', i, xCount, xName, xType)
+						if DEBUG then debug(debugString) end
+					end
+					else
+					break
 				end
-				else
-				break
 			end
 		end
 	end
@@ -80,7 +81,7 @@ local function loop()
 	while RUNNING do
 		mq.delay('1s')
 		if not PAUSED then
-			if mq.TLO.Me.CombatState() ~= 'COMBAT' then ScanXtar() end
+			ScanXtar()
 		end
 	end
 end
